64ea21b23dc97378bcdd674df699992a9d504889
arturazambuja <arturantoniolli@gmail.com>
2025-11-07 15:44:12 -0300

    teste: add all crud dao tests

--- src/test/java/DAO/TestEmprestimoDAO.java
+++ src/test/java/DAO/TestEmprestimoDAO.java
@@ -45,4 +45,54 @@ class TestEmprestimoDAO {
         assertEquals(10, result);
         verify(statementMock, times(1)).executeQuery("SELECT MAX(ide) ide FROM tb_emprestimo");
     }
+
+    @Test
+    void testGetListaEmprestimo() throws Exception {
+        when(connectionMock.createStatement()).thenReturn(statementMock);
+        when(statementMock.executeQuery(anyString())).thenReturn(resultSetMock);
+        when(resultSetMock.next()).thenReturn(true, false);
+        when(resultSetMock.getInt("ide")).thenReturn(1);
+        when(resultSetMock.getInt("quantidade")).thenReturn(2);
+        when(resultSetMock.getString("dataloc")).thenReturn("2025-01-01");
+        when(resultSetMock.getString("datadev")).thenReturn("2025-02-01");
+        when(resultSetMock.getString("status")).thenReturn("ativo");
+        when(resultSetMock.getInt("idc")).thenReturn(3);
+        when(resultSetMock.getInt("idf")).thenReturn(4);
+
+        ArrayList<Emprestimo> lista = dao.getListaEmprestimo();
+
+        assertEquals(1, lista.size());
+        Emprestimo e = lista.get(0);
+        assertEquals("ativo", e.getStatus());
+    }
+    
+    @Test
+    void testInsertEmprestimoBD() throws Exception {
+        Emprestimo emp = new Emprestimo(1, 5, "2025-01-01", "2025-02-01", "ativo", 2, 3);
+
+        when(connectionMock.prepareStatement(anyString())).thenReturn(preparedStatementMock);
+        boolean result = dao.InsertEmprestimoBD(emp);
+
+        assertTrue(result);
+        verify(preparedStatementMock, times(1)).execute();
+        verify(preparedStatementMock, times(1)).close();
+    }
+
+    @Test
+    void testDeleteEmprestimoBD() throws Exception {
+        when(connectionMock.createStatement()).thenReturn(statementMock);
+        boolean result = dao.DeleteEmprestimoBD(5);
+        assertTrue(result);
+        verify(statementMock, times(1)).executeUpdate(contains("DELETE FROM tb_emprestimo"));
+    }
+
+    @Test
+    void testUpdateEmprestimoBD() throws Exception {
+        Emprestimo emp = new Emprestimo(1, 2, "2025-01-01", "2025-02-01", "ativo", 1, 2);
+        when(connectionMock.prepareStatement(anyString())).thenReturn(preparedStatementMock);
+        boolean result = dao.UpdateEmprestimoBD(emp);
+
+        assertTrue(result);
+        verify(preparedStatementMock, times(1)).execute();
+    }
 }
